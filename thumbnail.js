d446b2b7e321c224a10f2eeba8622bc50cc5c73a
PrawKanit <65989264+PrawKanit@users.noreply.github.com>
2022-02-25 19:47:13 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,4 +1,67 @@
-let targetImage = document.querySelector("#smart-image");
+let targetImage = document.querySelector("#smart-image1");
+targetImage.addEventListener("click", function() {
+    if (targetImage.classList.contains("small")) {
+        targetImage.classList.remove("small");
+    }else {
+        targetImage.classList.add("small");
+    }
+});
+
+let targetImage = document.querySelector("#smart-image2");
+targetImage.addEventListener("click", function() {
+    if (targetImage.classList.contains("small")) {
+        targetImage.classList.remove("small");
+    }else {
+        targetImage.classList.add("small");
+    }
+});
+
+let targetImage = document.querySelector("#smart-image3");
+targetImage.addEventListener("click", function() {
+    if (targetImage.classList.contains("small")) {
+        targetImage.classList.remove("small");
+    }else {
+        targetImage.classList.add("small");
+    }
+});
+
+let targetImage = document.querySelector("#smart-image4");
+targetImage.addEventListener("click", function() {
+    if (targetImage.classList.contains("small")) {
+        targetImage.classList.remove("small");
+    }else {
+        targetImage.classList.add("small");
+    }
+});
+
+let targetImage = document.querySelector("#smart-image5");
+targetImage.addEventListener("click", function() {
+    if (targetImage.classList.contains("small")) {
+        targetImage.classList.remove("small");
+    }else {
+        targetImage.classList.add("small");
+    }
+});
+
+let targetImage = document.querySelector("#smart-image6");
+targetImage.addEventListener("click", function() {
+    if (targetImage.classList.contains("small")) {
+        targetImage.classList.remove("small");
+    }else {
+        targetImage.classList.add("small");
+    }
+});
+
+let targetImage = document.querySelector("#smart-image7");
+targetImage.addEventListener("click", function() {
+    if (targetImage.classList.contains("small")) {
+        targetImage.classList.remove("small");
+    }else {
+        targetImage.classList.add("small");
+    }
+});
+
+let targetImage = document.querySelector("#smart-image8");
 targetImage.addEventListener("click", function() {
     if (targetImage.classList.contains("small")) {
         targetImage.classList.remove("small");
